724ab032125f5c4b4b4389c53a4d51c5f77d64fd
johnsontaiwo <josseytee@gmail.com>
2017-01-05 19:12:25 -0500

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,9 @@
 class CartsController < ApplicationController
+ #before_action :set_cart, only: [:show, :checkout]
 
-  
 
   def show
-    @current_cart = current_user.current_cart
+    @current_cart =  current_user.current_cart
   end
   
 
@@ -11,17 +11,23 @@ class CartsController < ApplicationController
   
    @current_cart = current_user.current_cart
    
-   current_user.current_cart.line_items.each do |line_item|
+   @current_cart.line_items.each do |line_item|
     item = Item.find(line_item.item_id)
     item.inventory = (item.inventory - line_item.quantity)
     item.save
-    
+    #binding.pry
    end
 
    current_user.remove_cart
      
    
-   redirect_to cart_path(current_user)
+   redirect_to cart_path(@current_cart)
    
   end
+
+  private
+   
+   def set_cart
+    @current_cart = Cart.find(params[:id])
+   end
 end
